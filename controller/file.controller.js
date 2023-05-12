7775594155dd9b93b1d63306d402ccbd98f392e4
dholariyaravi <ravidholariya700@gmail.com>
2023-05-12 10:42:39 +0530

    update

--- controller/file.controller.js
+++ controller/file.controller.js
@@ -1,6 +1,6 @@
 const uploadFile = require("../middleware/upload");
 const fs = require("fs");
-const baseUrl = "http://localhost:5050s/files/";
+const baseUrl = "http://localhost:5050/files/";
 
 const upload = async (req, res) => {
   try {
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const controller = require("../confing/file.controller");
+const controller = require("../controller/file.controller");
 
 let routes = (app) => {
   router.post("/upload", controller.upload);
